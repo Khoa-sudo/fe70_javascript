52bd7da81ee7b9fe25f13c79d4c2156d3f82b933
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-07-04 17:01:50 +0700

    hoan thanh P10

---
+++ P8_DoiTuongChuoi/DoiTuongChuoi.js
@@ -0,0 +1,16 @@
+var chuoi = "Welcome to itclass.vn";
+// document.write(chuoi.replace('itclass.vn','blog.myclass.vn'))
+console.log(chuoi.replace("itclass.vn", "blog.myclass.vn"));
+
+//Chuyển chữ hoa thường
+// toUpperCase()
+// toLowerCase()
+
+console.log(chuoi.toUpperCase());
+
+// NỐI CHUỖI
+// - Sử dụng dấu "+"
+// - Sử dụng hàm String.concat()
+var chuoi = "<br> Welcome ";
+chuoi = chuoi.concat("to ", "myclass.vn");
+console.log(chuoi + " <br/>");
